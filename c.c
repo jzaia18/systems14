af3ea2d1ba7d89049940352e2a922821b8eb120f
TakingTheL <yliu6@stuy.edu>
2017-12-06 20:30:43 -0500

    we done

--- c.c
+++ c.c
@@ -14,15 +14,16 @@
 #define DEBUGGING 1 // Set to or as appropriate
 #define KEY 420
 
-int create_sem(char *arg ) {
+int create_sem(char *arg) {
   int n = atoi(arg);
   if ( n <= 0 )
     return -1;
-  int s = semget(KEY, 1, IPC_CREAT | IPC_EXCL);
+  int s = semget(KEY, 1, 0600 | IPC_CREAT | IPC_EXCL);
   if ( s == -1 )
     printf("Semaphore already exists.\n");
   else {
     printf("Semaphore created: %d\n", s);
+    printf("value: %d\n", n);
     semctl(s, 0, SETVAL, n);
   }
   return s;
@@ -35,13 +36,16 @@ void view_sem() {
 }
 
 void remove_sem() {
-
+  int s = semget(KEY, 1, IPC_CREAT);
+  semctl(s, 0, IPC_RMID);
+  printf("Removed semaphore");
 }
 
 int main(int argc, char* argv[]) {
-
-  if (!strcmp(argv[1], "-c"))
+  if (!strcmp(argv[1], "-c") && argc == 3 && argv[2]){ //first arg is -c, there is a second arg, and second arg is not null
+    printf("creating...\n");
     create_sem(argv[2]);
+  }
   else if (!strcmp(argv[1], "-v"))
     view_sem();
   else if (!strcmp(argv[1], "-r"))
